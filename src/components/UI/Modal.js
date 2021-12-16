5380841edd05232313a6180fd16819415e1a7631
Władysław Reginiewicz <wladyslaw.reginiewicz@gmail.com>
2021-12-16 20:38:47 +0100

    cart modal on keydown close
    cart modal closes on Escape keydown event

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -3,6 +3,15 @@ import classes from "./Modal.module.css";
 import ReactDOM from "react-dom";
 
 const Backdrop = (props) => {
+  window.addEventListener("keydown", (event) => {
+    const eventKey = event.key;
+
+    if (eventKey !== "Escape") {
+      return;
+    }
+    props.onClose();
+  });
+
   return <div className={classes.backdrop} onClick={props.onClose}></div>;
 };
 
